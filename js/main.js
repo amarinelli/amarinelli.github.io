1422fbe223573f2de4b3edbd660b1a4ca9cca3d6
Adam Marinelli <amarinelli@slack-corp.com>
2016-12-12 00:08:27 -0500

    Beautify

--- js/main.js
+++ js/main.js
@@ -1,15 +1,15 @@
 (function(angular) {
 
-  // The module code
-  angular
-    .module('angularApp', [])
-    .controller('MainController', ['$scope', '$http', MainController]); //use this array syntax for minification
+    // The module code
+    angular
+        .module('angularApp', [])
+        .controller('MainController', ['$scope', '$http', MainController]); //use this array syntax for minification
 
-  // The controller code
-  function MainController($scope, $http) {
+    // The controller code
+    function MainController($scope, $http) {
 
-    $scope.greeting = "Hello ...";
+        $scope.greeting = "Hello ...";
 
-  }
+    }
 
 })(window.angular);
